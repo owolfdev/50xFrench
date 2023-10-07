36307d8d0ee71366a68f3b97fe5bbdf5909e5b5b
owolfdev <owolfdev@gmail.com>
2023-10-07 16:37:15 +0700

    init

---
+++ components/phrase.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { PHRASES } from "@/data/french_phrases";
+
+interface Phrase {
+  phrase: string;
+  translation: string;
+}
+
+export function PhraseCard() {
+  //get phrase
+  function getTodaysPhrase(): Phrase {
+    // Get the index and date from local storage
+    const storedIndex = localStorage.getItem("phraseIndex");
+    const storedDate = localStorage.getItem("lastVisitedDate");
+    const currentDate = new Date().toISOString().split("T")[0]; // gets YYYY-MM-DD format
+
+    let index = 0; // default
+
+    if (storedDate !== currentDate || !storedIndex) {
+      // If the user hasn't visited today or is a first-time visitor
+      if (storedIndex) {
+        index = (parseInt(storedIndex, 10) + 1) % PHRASES.length; // Increment and loop if necessary
+      }
+      localStorage.setItem("phraseIndex", index.toString());
+      localStorage.setItem("lastVisitedDate", currentDate);
+    } else {
+      index = parseInt(storedIndex, 10);
+    }
+
+    return PHRASES[index];
+  }
+
+  //construct date
+  const getFormattedDateWithDay = (): string => {
+    const today: Date = new Date();
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    };
+    return today.toLocaleDateString("en-US", options);
+  };
+
+  return (
+    <div className="flex flex-col gap-12 p-2">
+      <div className="flex flex-col gap-0 p-2">
+        <div className="">{getFormattedDateWithDay()}</div>
+        <div className="text-xl ">Repeat this phrase 50 times.</div>
+        <div>If you don't feel fluent after that, repeat.</div>
+      </div>
+      <Card className="w-full">
+        <CardHeader>
+          {/* <div className="pb-4">{getFormattedDateWithDay()}</div>
+        <CardTitle>Repeat this phrase 50 times today.</CardTitle>
+        <CardDescription>
+          If you don't feel fluent after this, repeat.
+        </CardDescription> */}
+        </CardHeader>
+        <CardContent>
+          <div className="sm:p-6">
+            <p className="sm:text-3xl text-2xl">{getTodaysPhrase().phrase}</p>
+          </div>
+          <div className="sm:p-6 p-4">
+            <p className="text-muted-foreground">
+              {getTodaysPhrase().translation}
+            </p>
+          </div>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Previous</Button>
+          <Button>Next</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
---
+++ data/raw_data.mjs
@@ -0,0 +1,52 @@
+export const rawData = `
+1. **Il est impératif que nous trouvions une solution avant demain.** - It's imperative that we find a solution before tomorrow.
+2. **En dépit des nombreux obstacles, ils ont réussi.** - Despite the numerous obstacles, they succeeded.
+3. **Les résultats, bien que préliminaires, sont très prometteurs.** - The results, although preliminary, are very promising.
+4. **Si j'avais su la vérité, j'aurais agi différemment.** - Had I known the truth, I would have acted differently.
+5. **Je crains qu'il ne soit trop tard pour s'excuser.** - I fear it might be too late to apologize.
+6. **Il n'est pas exclu que je vienne te rejoindre en Espagne cet été.** - It's not out of the question that I might join you in Spain this summer.
+7. **Nul ne sait ce que l'avenir nous réserve.** - No one knows what the future holds for us.
+8. **Je doute qu'il puisse terminer le projet à temps.** - I doubt he can finish the project on time.
+9. **Elle parle comme si elle savait tout sur le sujet.** - She speaks as if she knows everything about the subject.
+10. **Quoi qu'il arrive, je serai toujours à tes côtés.** - Whatever happens, I'll always be by your side.
+11. **Les opinions divergent quant à la meilleure approche à adopter.** - Opinions vary on the best approach to take.
+12. **La beauté est dans l'œil de celui qui regarde.** - Beauty is in the eye of the beholder.
+13. **Le plus difficile n'est pas de commencer, mais de persévérer.** - The hardest part isn't starting but persevering.
+14. **À force de persévérance, il a finalement réussi.** - Through perseverance, he finally succeeded.
+15. **Ce n'est qu'en perdant tout qu'on est libre de tout entreprendre.** - It's only after we've lost everything that we're free to do anything.
+16. **Chaque échec est une marche vers le succès.** - Every failure is a step towards success.
+17. **On ne peut pas juger un livre à sa couverture.** - You can't judge a book by its cover.
+18. **Il faut battre le fer pendant qu'il est chaud.** - Strike while the iron is hot.
+19. **Le cœur a ses raisons que la raison ne connaît point.** - The heart has its reasons which reason knows not.
+20. **La fin justifie les moyens.** - The ends justify the means.
+21. **Ce qui ne nous tue pas nous rend plus fort.** - What doesn't kill us makes us stronger.
+22. **Le monde appartient à ceux qui se lèvent tôt.** - The world belongs to those who get up early.
+23. **Mieux vaut tard que jamais.** - Better late than never.
+24. **On ne peut pas avoir le beurre et l'argent du beurre.** - You can't have your cake and eat it too.
+25. **Il faut tourner sept fois sa langue dans sa bouche avant de parler.** - Think before you speak.
+26. **L'habit ne fait pas le moine.** - Clothes don't make the man.
+27. **L'exception confirme la règle.** - The exception proves the rule.
+28. **Il n'y a pas de fumée sans feu.** - Where there's smoke, there's fire.
+29. **On ne fait pas d'omelette sans casser des œufs.** - You can't make an omelette without breaking eggs.
+30. **Tout est bien qui finit bien.** - All's well that ends well.
+31. **Qui veut la fin veut les moyens.** - He who wills the end wills the means.
+32. **Rome ne s'est pas faite en un jour.** - Rome wasn't built in a day.
+33. **Qui se ressemble s'assemble.** - Birds of a feather flock together.
+34. **Chassez le naturel, il revient au galop.** - What's bred in the bone will come out in the flesh.
+35. **Il est difficile d'enseigner de vieux singes à faire de nouvelles acrobaties.** - It's hard to teach an old dog new tricks.
+36. **Les grands esprits se rencontrent.** - Great minds think alike.
+37. **La nuit, tous les chats sont gris.** - In the dark, all cats are grey.
+38. **Loin des yeux, loin du cœur.** - Out of sight, out of mind.
+39. **Il vaut mieux prévenir que guérir.** - An ounce of prevention is worth a pound of cure.
+40. **Qui vivra verra.** - Time will tell.
+41. **La vérité éclate toujours au grand jour.** - The truth always comes out.
+42. **Le temps est le meilleur juge.** - Time is the best judge.
+43. **La fortune sourit aux audacieux.** - Fortune favours the bold.
+44. **L'appétit vient en mangeant.** - The more you have, the more you want.
+45. **C'est en forgeant qu'on devient forgeron.** - Practice makes perfect.
+46. **Il faut prendre la vie du bon côté.** - You have to look on the bright side of life.
+47. **Tous les chemins mènent à Rome.** - All roads lead to Rome.
+48. **On ne peut pas plaire à tout le monde.** - You can't please everyone.
+49. **La meilleure défense, c'est l'attaque.** - The best defense is a good offense.
+50. **Les actes parlent plus fort que les mots.** - Actions speak louder than words.
+`;
